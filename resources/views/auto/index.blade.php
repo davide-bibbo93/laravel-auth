f47f8cdaebf6a4946f21845df47787646c85d6d1
Davide <bibbodavide@gmail.com>
2021-03-25 22:49:58 +0100

    modifiche

--- resources/views/auto/index.blade.php
+++ resources/views/auto/index.blade.php
@@ -13,6 +13,15 @@
                 <th scope="col">Velocità massima</th>
                 <th scope="col">Immagine</th>
                 <th scope="col">Dettagli</th>
+                @auth
+                    <th scope="col">
+                        <a href="{{ route('auto.create') }}">
+                            <button class="btn btn-light">
+                                <i class="fas fa-plus">Crea Macchina</i>
+                            </button>
+                        </a>
+                    </th>
+                @endauth
             </tr>
         </thead>
         <tbody>
@@ -31,24 +40,43 @@
                             </button>
                         </a>
 
-                        @if (Auth::check())
+                        @auth
+
                             <a href="{{ route('auto.edit', ['auto' => $car]) }}">
                                 <button class="btn btn-success">
                                     <i class="fas fa-edit"></i>
                                 </button>
                             </a>
 
-                            <form action="{{ route('auto.destroy', ['auto' => $car]) }}" method="post">
-                                @csrf
-                                @method('DELETE')
-
-                                <button type="submit" class="btn btn-danger">
-                                    <i class="fas fa-trash-alt"></i>
-                                </button>
-
-                            </form>
-                        @endif
-
+                            <button type="button" class="btn btn-danger" data-toggle="modal"
+                                data-target="#exampleModalCenter{{ $car->id }}">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                            <div class="modal fade" id="exampleModalCenter{{ $car->id }}" tabindex="-1" role="dialog"
+                                aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLongTitle">Cancellazione</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Sei sicuro di voler cancellare definitivamente questa macchina?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Chiudi</button>
+                                            <form action="{{ route('auto.destroy', ['auto' => $car]) }}" method="post">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Cancella</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endauth
                     </td>
                 </tr>
             @endforeach
--- resources/views/template/base.blade.php
+++ resources/views/template/base.blade.php
@@ -1,24 +1,59 @@
 <html>
 
-<head>
-    <title>laravel-auth - @yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css">
-    <script src="{{ asset('js/app.js') }}"></script>
+    <head>
+        <title>laravel-auth - @yield('title')</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css">
+        <script src="{{ asset('js/app.js') }}"></script>
+    </head>
 
-</head>
+    <body>
 
-<body>
+        <header>
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('public.auto.index') }}" style="color: white;">
+                                    <h4>Indice</h4>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/" style="color: white;">
+                                    <h4>Home</h4>
+                                </a>
+                            </li>
+                        </ul>
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                        @if (!Auth::check())
+                            <a class="btn btn-success" href="/login">Login</a>
+                        @else
+                            <div class="btn btn-danger" aria-labelledby="navbarDropdown">
+                                <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        @endif
+                    </div>
+                </nav>
+            </div>
+        </header>
 
-    <header>
         <div class="container">
-            <h1><a href="{{ route('public.auto.index') }}">Concessionario Auto</a></h1>
+            @yield('content')
         </div>
-    </header>
 
-    <div class="container">
-        @yield('content')
-    </div>
-
-</body>
+    </body>
 
 </html>
